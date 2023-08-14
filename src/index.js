dc0348089d52c09ec2dad3bef10f3573ba3003e8
Artur Shynkaryk <mun9je@gmail.com>
2023-08-14 20:28:38 +0300

    feat:add answer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
+module.exports = function towelSort(matrix) {
+  if (!matrix || matrix.length === 0) {
+    return [];
+  }
 
-console.log(
-  matrix.reduce((acc, cur, i) => {
-    cur.sort((a, b) => !(i & 1) ? a - b : b - a).map(e => acc.push(e));
-    return acc;
-  }, [])
-);
+  const result = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    if (i % 2 === 0) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        result.push(matrix[i][j]);
+      }
+    } else {
+      for (let j = matrix[i].length - 1; j >= 0; j--) {
+        result.push(matrix[i][j]);
+      }
+    }
+  }
+
+  return result;
+};
